5f29d3705ba5132c8878d0acc699b1de81331d17
Marcos Lutero <lutero_rocha@outlook.com>
2024-11-20 17:56:00 -0300

    corrigindo a entidade categoria

--- src/main/java/com/desafio2/eventos/entities/Categoria.java
+++ src/main/java/com/desafio2/eventos/entities/Categoria.java
@@ -2,6 +2,7 @@ package com.desafio2.eventos.entities;
 
 
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -49,10 +50,24 @@ public class Categoria {
 		this.atividades = atividades;
 	}
 
-	public Categoria(Long id) {
-		super();
-		this.id = id;
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
 	
 	
 	
